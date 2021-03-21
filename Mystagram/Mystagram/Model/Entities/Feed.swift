d7e8d16c11c758c2a072e9878bfd24fe3d89c7f3
SEONGJUN <naroti@naver.com>
2021-03-21 15:43:44 +0900

    add delegateproxy for CLLocationManager

--- Mystagram/Mystagram/Model/Entities/Feed.swift
+++ Mystagram/Mystagram/Model/Entities/Feed.swift
@@ -5,7 +5,9 @@
 //  Created by SEONGJUN on 2021/03/17.
 //
 
-import Foundation
+import MapKit
+import RxSwift
+import RxCocoa
 
 struct Feed {
     let user: User
@@ -14,4 +16,35 @@ struct Feed {
     let posetedText: String
     let likeCount: Int
     let reply: [Reply]
+}
+
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
+class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    
+    weak private(set) var locationManager: CLLocationManager?
+    
+    init(locationManager: CLLocationManager) {
+        self.locationManager = locationManager
+        super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+    }
+    
+    static func registerKnownImplementations() {
+        self.register {
+            RxCLLocationManagerDelegateProxy(locationManager: $0)
+        }
+    }
+}
+
+extension Reactive where Base: CLLocationManager {
+    var delegate: DelegateProxy<CLLocationManager,CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: base)
+    }
+    
+    var didUpdateLocation: Observable<CLLocation> {
+        return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+            .map { $0[1] as! CLLocation }
+    }
 }
